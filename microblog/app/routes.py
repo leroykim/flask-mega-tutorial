968a48bd978f5199dec3269e39f4df86703eed5c
Dae-young Kim <leroy.kim@umbc.edu>
2022-02-24 00:05:00 -0500

    Complete chapter 9

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -27,16 +27,33 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
 
-    posts = current_user.followed_posts().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
 
-    return render_template('index.jinja', title='Home', form=form, posts=posts)
+    return render_template('index.jinja', title='Home', form=form, 
+                            posts=posts.items, next_url=next_url,
+                            prev_url=prev_url)
 
 
-@app.route('/exlore')
+@app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('Index.jinja', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('Index.jinja', title='Explore', posts=posts.items,
+                            next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -85,11 +102,20 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test Post #1'},
-        {'author': user, 'body': 'Test Post #2'}
-    ]
-    return render_template('user.jinja', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('user', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template(
+        'user.jinja', 
+        user=user, 
+        posts=posts.items,
+        next_url=next_url, prev_url=prev_url
+    )
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
